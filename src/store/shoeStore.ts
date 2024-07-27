50bcacb677e74152718a6cbf3656bcf5e6c2ac53
Ranjith.Kumar <theranjith@gmail.com>
2024-07-27 08:25:12 +0530

    added persist local storage in zustand

--- src/store/shoeStore.ts
+++ src/store/shoeStore.ts
@@ -1,5 +1,6 @@
 import { CartItem, User } from "../types/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type State = {
   user: User | null;
@@ -14,30 +15,37 @@ type Action = {
   addToCart: (id: number, sku: string) => void;
 };
 
-export const useShoeStore = create<State & Action>((set) => ({
-  user: null,
-  cart: [],
-  logIn: (user: User) => set({ user }),
-  logOut: () => set({ user: null }),
-  emptyCart: () => set({ cart: [] }),
-  updateCartQuantity: (sku: string, quantity: number) => {
-    set(({ cart }) => ({
-      cart:
-        quantity === 0
-          ? cart.filter((i) => i.sku !== sku)
-          : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i)),
-    }));
-  },
-  addToCart: (id: number, sku: string) => {
-    set(({ cart }) => {
-      const itemInCart = cart.find((i) => i.sku === sku);
-      return {
-        cart: itemInCart
-          ? cart.map((item) =>
-              item.sku === sku ? { ...item, quantity: item.quantity + 1 } : item
-            )
-          : [...cart, { id, sku, quantity: 1 }],
-      };
-    });
-  },
-}));
+export const useShoeStore = create<State & Action>()(
+  persist<State & Action>(
+    (set) => ({
+      user: null,
+      cart: [],
+      logIn: (user: User) => set({ user }),
+      logOut: () => set({ user: null }),
+      emptyCart: () => set({ cart: [] }),
+      updateCartQuantity: (sku: string, quantity: number) => {
+        set(({ cart }) => ({
+          cart:
+            quantity === 0
+              ? cart.filter((i) => i.sku !== sku)
+              : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i)),
+        }));
+      },
+      addToCart: (id: number, sku: string) => {
+        set(({ cart }) => {
+          const itemInCart = cart.find((i) => i.sku === sku);
+          return {
+            cart: itemInCart
+              ? cart.map((item) =>
+                  item.sku === sku
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+                )
+              : [...cart, { id, sku, quantity: 1 }],
+          };
+        });
+      },
+    }),
+    { name: "zustand-store" }
+  )
+);
